b44cee818386e2c711e7fad43b77d82c575b56f8
trichner <github@trichner.ch>
2015-12-21 17:44:13 +0100

    forgot to commit *.cpp :/

--- llvm-patches/llvm_intrinsics.cpp
+++ llvm-patches/llvm_intrinsics.cpp
@@ -12,11 +12,6 @@ const LLVMIntrinsicID LLVMIntrinsicIDMemset = llvm::Intrinsic::memset;
 const LLVMIntrinsicID LLVMIntrinsicIDMemmove = llvm::Intrinsic::memmove;
 const LLVMIntrinsicID LLVMIntrinsicIDMemcpy = llvm::Intrinsic::memcpy;
 
-
-/*
- * The Tys parameter is for intrinsics with overloaded types (e.g., those using iAny, fAny, vAny, or iPTRAny).
- * For a declaration of an overloaded intrinsic, Tys must provide exactly one type for each overloaded type in the intrinsic.
- */
 LLVMValueRef LLVMGetIntrinsicByID(LLVMModuleRef mod, LLVMIntrinsicID id, LLVMTypeRef *tys,int nty){
 	std::vector<llvm::Type *> arg_types;
 	for(;nty>0;nty--){
